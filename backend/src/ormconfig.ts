6229ece2a0ee8401fd186a1961d9290b6bd14922
Denis <denis@shushin.io>
2021-09-01 22:11:32 +0300

    docker setup for backend

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -2,8 +2,8 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5454,
+  host: 'db',
+  port: 5432,
   username: 'postgres',
   password: 'postgres',
   database: 'linksnest',
