fdd355260ea79fa8f7a67f326d78e0b1e80c240e
Denis <denis@shushin.io>
2021-09-11 01:15:24 +0300

    react with TS added to frontend

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { LogInWithCredentialsGuard } from '../auth/logInWithCredentials.guard';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('user/:id')
+  @UseGuards(LogInWithCredentialsGuard)
   getById(@Param('id') id: string) {
     return this.usersService.getById(+id);
   }
